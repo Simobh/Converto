886d28169914d4be03d7b9f7723d6aea3f4a83e8
Rayane Saadi <rayanesaadi@MacBook-Air-de-Rayane.local>
2025-02-14 12:04:03 +0100

    Rayane commit : style css

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
-@Component({ selector: 'app-signup', templateUrl: './signup.component.html' })
+@Component({ 
+  selector: 'app-signup', 
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
 export class SignupComponent {
   email = '';
   password = '';
+  name ='';
 
   constructor(private authService: AuthService) {}
   signUp() { this.authService.signUp(this.email, this.password); }
